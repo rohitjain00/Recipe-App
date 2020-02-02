3c902d7e44f3e46c897adb431213ad232ca5b73b
Rohit Jain <rohitjain18005@gmail.com>
2020-02-02 22:45:00 +0530

    local storage service created

--- src/app/local-storage.service.ts
+++ src/app/local-storage.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import {LOCAL_STORAGE, StorageService} from 'ngx-webstorage-service';
+
+
+let STORAGE_KEY: string;
+STORAGE_KEY = 'user-auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalStorageService {
 
-  constructor() { }
+  constructor(@Inject(LOCAL_STORAGE) private storageService: StorageService) { }
+
+  public setAuthenticationToken(authenticationToken: String) {
+    this.storageService.set(STORAGE_KEY, authenticationToken);
+  }
+  public isLoggedIn() {
+    return this.storageService.has(STORAGE_KEY);
+  }
+  public removeUser() {
+    this.storageService.remove(STORAGE_KEY);
+  }
+  public getAuthenticationToken() {
+    return this.storageService.get(STORAGE_KEY)
+  }
 }
